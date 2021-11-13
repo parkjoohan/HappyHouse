81294da7edac1815b4607c7cc241fb435e5cef33
alex0317@naver.com <joohanpark1513@gmail.com>
2021-11-14 00:36:09 +0900

    지도 제작 중

---
+++ frontend_happyhouse/src/components/common/Nav.vue
@@ -0,0 +1,32 @@
+<template>
+  <img class="slide-image" :src="getImgUrl(images[0])" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      images: ["/assets/1.jpg", "/assets/2.jpg", "/assets/3.jpg"],
+    };
+  },
+  watch: {
+    getImgUrl() {},
+  },
+  mounted() {
+    window.setInterval(() => {
+      this.slide();
+    }, 5000);
+  },
+  methods: {
+    getImgUrl(im) {
+      return require("../assets" + im);
+    },
+    slide() {
+      let first = this.images.shift();
+      this.images = this.images.concat(first);
+    },
+  },
+};
+</script>
+
+<style></style>
