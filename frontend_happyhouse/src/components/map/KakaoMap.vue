313e24583e47ae57e45bce2fbba3b2c49d536c57
alex0317@naver.com <joohanpark1513@gmail.com>
2021-11-16 02:27:27 +0900

    확대,축소 / 지도 변환 구현

--- frontend_happyhouse/src/components/map/KakaoMap.vue
+++ frontend_happyhouse/src/components/map/KakaoMap.vue
@@ -5,15 +5,31 @@
     <div hidden>
       {{ aptlist }}
     </div>
+    <p>
+      <input
+        type="checkbox"
+        id="chkUseDistrict"
+        @click="setOverlayMapTypeId()"
+      />
+      지적편집도 정보 보기
+      <input type="checkbox" id="chkTerrain" @click="setOverlayMapTypeId()" />
+      지형정보 보기
+      <input type="checkbox" id="chkTraffic" @click="setOverlayMapTypeId()" />
+      교통정보 보기
+      <input type="checkbox" id="chkBicycle" @click="setOverlayMapTypeId()" />
+      자전거도로 정보 보기
+    </p>
   </div>
 </template>
 
 <script>
+let map;
 export default {
   name: "KakaoMap",
   data() {
     return {
       container: Document,
+      infowindow: "",
     };
   },
   props: {
@@ -35,12 +51,24 @@ export default {
         center: new kakao.maps.LatLng(33.450701, 126.570667),
         level: 3,
       };
-      let map = new kakao.maps.Map(this.container, options);
+      map = new kakao.maps.Map(this.container, options);
       //마커추가하려면 객체를 아래와 같이 하나 만든다.
       var marker = new kakao.maps.Marker({
         position: map.getCenter(),
       });
       marker.setMap(map);
+      // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+      var mapTypeControl = new kakao.maps.MapTypeControl();
+
+      ////////////////////////////////////////////확대 / 축소 막대////////////////////////////////////////////////////
+
+      // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+      // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+      map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+      // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+      var zoomControl = new kakao.maps.ZoomControl();
+      map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
     },
     addScript() {
       const script = document.createElement("script"); /* global kakao */
@@ -49,6 +77,46 @@ export default {
         "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=b433b7b8f851f9709d26a15fb4fc0660&libraries=services";
       document.head.appendChild(script);
     },
+    // 지도 변환
+    setOverlayMapTypeId() {
+      var mapTypes = {
+        terrain: kakao.maps.MapTypeId.TERRAIN,
+        traffic: kakao.maps.MapTypeId.TRAFFIC,
+        bicycle: kakao.maps.MapTypeId.BICYCLE,
+        useDistrict: kakao.maps.MapTypeId.USE_DISTRICT,
+      };
+
+      var chkTerrain = document.getElementById("chkTerrain"),
+        chkTraffic = document.getElementById("chkTraffic"),
+        chkBicycle = document.getElementById("chkBicycle"),
+        chkUseDistrict = document.getElementById("chkUseDistrict");
+
+      // 지도 타입을 제거합니다
+      for (var type in mapTypes) {
+        map.removeOverlayMapTypeId(mapTypes[type]);
+      }
+
+      // 지적편집도정보 체크박스가 체크되어있으면 지도에 지적편집도정보 지도타입을 추가합니다
+      if (chkUseDistrict.checked) {
+        map.addOverlayMapTypeId(mapTypes.useDistrict);
+      }
+
+      // 지형정보 체크박스가 체크되어있으면 지도에 지형정보 지도타입을 추가합니다
+      if (chkTerrain.checked) {
+        map.addOverlayMapTypeId(mapTypes.terrain);
+      }
+
+      // 교통정보 체크박스가 체크되어있으면 지도에 교통정보 지도타입을 추가합니다
+      if (chkTraffic.checked) {
+        map.addOverlayMapTypeId(mapTypes.traffic);
+      }
+
+      // 자전거도로정보 체크박스가 체크되어있으면 지도에 자전거도로정보 지도타입을 추가합니다
+      if (chkBicycle.checked) {
+        map.addOverlayMapTypeId(mapTypes.bicycle);
+      }
+    },
+
     addMarker() {
       var mapOption = {
         center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
@@ -128,3 +196,158 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.map_wrap,
+.map_wrap * {
+  margin: 0;
+  padding: 0;
+  font-family: "Malgun Gothic", dotum, "돋움", sans-serif;
+  font-size: 12px;
+}
+.map_wrap a,
+.map_wrap a:hover,
+.map_wrap a:active {
+  color: #000;
+  text-decoration: none;
+}
+.map_wrap {
+  position: relative;
+  width: 100%;
+  height: 500px;
+}
+#menu_wrap {
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  width: 250px;
+  margin: 10px 0 30px 10px;
+  padding: 5px;
+  overflow-y: auto;
+  background: rgba(255, 255, 255, 0.7);
+  z-index: 1;
+  font-size: 12px;
+  border-radius: 10px;
+}
+.bg_white {
+  background: #fff;
+}
+#menu_wrap hr {
+  display: block;
+  height: 1px;
+  border: 0;
+  border-top: 2px solid #5f5f5f;
+  margin: 3px 0;
+}
+#menu_wrap .option {
+  text-align: center;
+}
+#menu_wrap .option p {
+  margin: 10px 0;
+}
+#menu_wrap .option button {
+  margin-left: 5px;
+}
+#placesList li {
+  list-style: none;
+}
+#placesList .item {
+  position: relative;
+  border-bottom: 1px solid #888;
+  overflow: hidden;
+  cursor: pointer;
+  min-height: 65px;
+}
+#placesList .item span {
+  display: block;
+  margin-top: 4px;
+}
+#placesList .item h5,
+#placesList .item .info {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+}
+#placesList .item .info {
+  padding: 10px 0 10px 55px;
+}
+#placesList .info .gray {
+  color: #8a8a8a;
+}
+#placesList .info .jibun {
+  padding-left: 26px;
+  background: url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/places_jibun.png)
+    no-repeat;
+}
+#placesList .info .tel {
+  color: #009900;
+}
+#placesList .item .markerbg {
+  float: left;
+  position: absolute;
+  width: 36px;
+  height: 37px;
+  margin: 10px 0 0 10px;
+  background: url(https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png)
+    no-repeat;
+}
+#placesList .item .marker_1 {
+  background-position: 0 -10px;
+}
+#placesList .item .marker_2 {
+  background-position: 0 -56px;
+}
+#placesList .item .marker_3 {
+  background-position: 0 -102px;
+}
+#placesList .item .marker_4 {
+  background-position: 0 -148px;
+}
+#placesList .item .marker_5 {
+  background-position: 0 -194px;
+}
+#placesList .item .marker_6 {
+  background-position: 0 -240px;
+}
+#placesList .item .marker_7 {
+  background-position: 0 -286px;
+}
+#placesList .item .marker_8 {
+  background-position: 0 -332px;
+}
+#placesList .item .marker_9 {
+  background-position: 0 -378px;
+}
+#placesList .item .marker_10 {
+  background-position: 0 -423px;
+}
+#placesList .item .marker_11 {
+  background-position: 0 -470px;
+}
+#placesList .item .marker_12 {
+  background-position: 0 -516px;
+}
+#placesList .item .marker_13 {
+  background-position: 0 -562px;
+}
+#placesList .item .marker_14 {
+  background-position: 0 -608px;
+}
+#placesList .item .marker_15 {
+  background-position: 0 -654px;
+}
+#pagination {
+  margin: 10px auto;
+  text-align: center;
+}
+#pagination a {
+  display: inline-block;
+  margin-right: 10px;
+}
+#pagination .on {
+  font-weight: bold;
+  cursor: default;
+  color: #777;
+}
+</style>
--- frontend_happyhouse/src/views/House.vue
+++ frontend_happyhouse/src/views/House.vue
@@ -4,9 +4,7 @@
       <h3 class="underline-orange">
         <b-icon icon="house-fill"></b-icon> House Service
       </h3>
-
       <kakao-map></kakao-map>
-
       <b-row>
         <b-col>
           <house-search-bar></house-search-bar>
