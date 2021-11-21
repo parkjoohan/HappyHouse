a0ca932ec7e88ec81a86886514bd08fe74fadb2a
alex0317@naver.com <joohanpark1513@gmail.com>
2021-11-21 14:50:04 +0900

    Vue 제작 중

--- frontend_happyhouse/src/views/Home.vue
+++ frontend_happyhouse/src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
-    <div class="bg">
+    <div class="bg1">
       <video muted autoplay loop>
         <source src="@/assets/City.mp4" type="video/mp4" />
       </video>
       <div class="text">
-        <p style="font-size: 30px">
+        <p style="font-size: 40px">
           Nothing is more relaxing than chilling at home.<br />
           There is nothing like staying at home for real comfort.
         </p>
+        <p style="font-size: 30px">- Jane Austen -</p>
       </div>
     </div>
 
@@ -47,6 +48,30 @@
         <h4>바다와 먹거리의 중심!</h4>
       </section>
     </div>
+
+    <div class="container">
+      <section class="item item-b" width="30%" align="center">
+        <h1>대전</h1>
+        <h4>과학의 도시, 미래의 도시!!</h4>
+      </section>
+      <section class="item item-a">
+        <!-- <h1>Section a</h1>
+        <p>hello woreld</p> -->
+        <img src="@/assets/daejun.jpg" width="100%" alt="" />
+      </section>
+    </div>
+
+    <div class="container">
+      <section class="item item-a">
+        <!-- <h1>Section a</h1>
+        <p>hello woreld</p> -->
+        <img src="@/assets/gumi.jpg" width="100%" alt="" />
+      </section>
+      <section class="item item-b" width="30%" align="center">
+        <h1>구미</h1>
+        <h4>당신(시민)과 함께라서 신나는 스마트 구미</h4>
+      </section>
+    </div>
   </div>
 </template>
 
@@ -59,7 +84,7 @@ body {
   padding: 0px;
   margin: 0px;
 }
-.bg {
+.bg1 {
   width: 100%;
   height: 500px;
   overflow: hidden;
@@ -78,7 +103,7 @@ video {
 }
 .text p {
   text-align: center;
-  font-size: 60px;
+  font-size: 100px;
   color: #ffffff;
 }
 .item {
