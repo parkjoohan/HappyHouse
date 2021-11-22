2083fd5a6b0b187e86a5697ffe25e97385aa38d6
alex0317@naver.com <joohanpark1513@gmail.com>
2021-11-22 10:51:48 +0900

    아파트 상세리스트, 테이블로 변경

--- frontend_happyhouse/src/components/house/HouseDetail.vue
+++ frontend_happyhouse/src/components/house/HouseDetail.vue
@@ -10,7 +10,41 @@
         ><b-img :src="require('@/assets/apt.png')" fluid-grow></b-img
       ></b-col>
     </b-row>
-    <b-row>
+    <table class="info-table">
+      <thead>
+        <tr>
+          <th scope="cols">구분</th>
+          <th scope="cols">정보</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row" class="even">거래일자</th>
+          <td class="even">
+            {{ house.년 }}년 {{ house.월 }}월 {{ house.일 }}일
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">주소</th>
+          <td>{{ house.법정동 }} {{ house.지번 }}</td>
+        </tr>
+        <tr>
+          <th scope="row" class="even">층수</th>
+          <td class="even">{{ house.층 }}층</td>
+        </tr>
+        <tr>
+          <th scope="row">전용면적</th>
+          <td>{{ house.전용면적 }}m<sup>2</sup></td>
+        </tr>
+        <tr>
+          <th scope="row" class="even">거래금액</th>
+          <td class="even">
+            {{ (parseInt(house.거래금액.replace(",", "")) * 10000) | price }}원
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <!-- <b-row>
       <b-col>
         <b-alert show variant="secondary"
           >일련번호 : {{ house.일련번호 }}</b-alert
@@ -52,7 +86,7 @@
           }}원</b-alert
         >
       </b-col>
-    </b-row>
+    </b-row> -->
   </b-container>
 </template>
 
@@ -78,4 +112,30 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+#field {
+  margin-top: 2%;
+  margin-bottom: 5%;
+}
+table.info-table {
+  border-collapse: collapse;
+  text-align: center;
+  line-height: 3;
+  border-top: 1px solid #ccc;
+  border-bottom: 1px solid #ccc;
+  width: 100%;
+  height: 30%;
+}
+table.info-table thead th {
+  width: 45%;
+  padding: 0px;
+  font-weight: bold;
+  vertical-align: top;
+  color: #fff;
+  background: #6c757d;
+  margin: 20px 10px;
+}
+table.info-table .even {
+  background: #c5c5c5;
+}
+</style>
